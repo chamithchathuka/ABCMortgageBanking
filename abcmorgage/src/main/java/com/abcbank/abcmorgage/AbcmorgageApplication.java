7f1d4d1cd6b5155d24392521572b4b510e93bad5
Chamith Chathuka <chamith.chathuka@gmail.com>
2024-12-07 13:12:46 +0800

    init
    project creation

---
+++ abcmorgage/src/main/java/com/abcbank/abcmorgage/AbcmorgageApplication.java
@@ -0,0 +1,13 @@
+package com.abcbank.abcmorgage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcmorgageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcmorgageApplication.class, args);
+	}
+
+}
---
+++ abcmorgage/src/test/java/com/abcbank/abcmorgage/AbcmorgageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcbank.abcmorgage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcmorgageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
