afe6f74dacfda0e7002c5c76092c7346502e7df0
Periyanayaki245 <periyanayaki.chandrasekaran@gmail.com>
2024-12-07 19:38:05 +0800

    Fixes

--- abcmorgage/src/main/java/com/abcbank/abcmorgage/controller/TransactionController.java
+++ abcmorgage/src/main/java/com/abcbank/abcmorgage/controller/TransactionController.java
@@ -1,5 +1,26 @@
 package com.abcbank.abcmorgage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abcbank.abcmorgage.model.TransactionStatus;
+import com.abcbank.abcmorgage.model.TransferModel;
+import com.abcbank.abcmorgage.service.TransferService;
+
+
+
+@RestController
 public class TransactionController {
 
+	@Autowired
+	private TransferService transferservice;
+	
+    @PostMapping("/transfer")
+	public TransactionStatus transferamount(@RequestBody TransferModel transfermodel){
+		return transferservice.transService(transfermodel);
+	}
+	
 }
